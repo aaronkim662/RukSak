366edc7e6b879181787d59023ed8863de8a07070
Aaronkim662 <aaronkim662@gmail.com>
2019-09-16 17:17:35 -0400

    Functional backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const { User } = require('../models');
+
+const { User } = require('../model');
 const { hashPassword, genToken, checkPassword } = require('../services/auth');
+const { restrict } = require('../services/auth');
 
 const userController = express.Router();
 
@@ -10,10 +12,18 @@ const buildAuthResponse = (user) => {
   const token = genToken(tokenData);
   return { user: { id, username }, token };
 };
-
+userController.get('/', async (req, res, next) => {
+  try {
+    const user = await User.findAll();
+    res.json(user);
+  } catch (e) {
+    next(e);
+  }
+});
 userController.post('/register', async (req, res, next) => {
   try {
     const { username, password } = req.body;
+    // eslint-disable-next-line
     const password_digest = await hashPassword(password);
     const user = await User.create({ username, password_digest });
     const respData = buildAuthResponse(user);
@@ -31,13 +41,18 @@ userController.post('/login', async (req, res, next) => {
     });
     if (await checkPassword(password, user.password_digest)) {
       const respData = buildAuthResponse(user);
-      res.json(respData);
+      res.json({ user: respData });
     } else {
       res.status(401).send('invalid Credentials');
     }
   } catch (e) {
     next(e);
   }
+});
+
+userController.delete('/login/:id', restrict, async (req, res) => {
+  const user = await User.findByPk(req.params.id);
+  await user.destroy();
 });
 
 module.exports = userController;
--- server.js
+++ server.js
@@ -6,30 +6,20 @@ const logger = require('morgan');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const gearController = require('./controllers/gearController');
+const locationController = require('./controllers/locationController');
+const userController = require('./controllers/userController');
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-// app.use((e, req, res) => {
-//   if (e) {
-//     console.log(e.message); // eslint-disable-line
-//     res.status(500).send(e.message);
-//   }
-// });
-
 app.get('/', (req, res) => {
   res.send('Welcome to the default route. 3001');
 });
-
-const locationController = require('./controllers/locationController')
 app.use('/location', locationController);
-
-const gearController = require('./controllers/gearController')
 app.use('/gear', gearController);
-
-const userController = require('./controllers/userController')
 app.use('/user', userController);
-
 // eslint-disable-next-line
 app.listen(PORT, () => console.log('up and running'));
