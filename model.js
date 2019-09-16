0f7f8fd76302bb6b097b9fcdbb9981a96980b948
Aaronkim662 <aaronkim662@gmail.com>
2019-09-16 14:11:49 -0400

    Fixed small errors

--- model.js
+++ model.js
@@ -4,52 +4,53 @@ const db = new Sequelize({
   database: 'ruksak_db',
   dialect: 'postgres',
   define: {
-    underscored: true
-  }
+    underscored: true,
+  },
 });
 
 class Gear extends Sequelize.Model {}
 Gear.init({
-  gear: Sequelize.STRING
+  gear: Sequelize.STRING,
 }, {
   sequelize: db,
-  modelName: 'gear'
+  modelName: 'gear',
 });
-
-Gear.belongsToMany(Type, { through: 'trip_gear' });
-Trip.belongsToOne(Gear, { through: 'trip_gear' });
 
 class Location extends Sequelize.Model {}
 Location.init({
-  location: Sequelize.STRING
+  location: Sequelize.STRING,
 }, {
   sequelize: db,
-  modelName: 'location'
+  modelName: 'location',
 });
 
 class Trip extends Sequelize.Model {}
 Trip.init({
-  trip: Sequelize.STRING
+  trip: Sequelize.STRING,
 }, {
   sequelize: db,
-  modelName: 'trip'
+  modelName: 'trip',
 });
-
-Trip.belongsToMany(User, { through: 'user_trips' });
-User.belongsToOne(Trip, { through: 'user_tips' });
 
 class User extends Sequelize.Model {}
 User.init({
   username: Sequelize.STRING,
-  password_digest: Sequelize.STRING
+  password_digest: Sequelize.STRING,
 }, {
   sequelize: db,
-  modelName: 'user'
+  modelName: 'user',
 });
+
+Trip.belongsToMany(User, { through: 'user_trips' });
+User.belongsToOne(Trip, { through: 'user_trips' });
+
+Gear.belongsToMany(Trip, { through: 'trip_gear' });
+Trip.belongsToOne(Gear, { through: 'trip_gear' });
+
 
 module.exports = {
   db,
   Gear,
   Location,
-  User
+  User,
 };
