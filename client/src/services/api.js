05e5b0e55de8bab15230db75b586bb592777473c
Aaronkim662 <aaronkim662@gmail.com>
2019-09-16 18:51:45 -0400

    Changes to api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,1 +1,37 @@
 const axios = require('axios');
+
+const BASE_URL = 'http://localhost:3001/';
+
+export const oneLocation = async (location) => {
+  let res = await axios.get(`${BASE_URL}/${location}`);
+  return res.data;
+}
+
+export const allGear = async () => {
+  let res = await axios.get(`${BASE_URL}/gear`);
+  return res.data;
+}
+
+export const oneGear = async (gearId) => {
+  let res = await axios.get(`${BASE_URL}/gear/${gearId}`);
+  return res.data;
+}
+
+export const deleteGear = async (gearId) => {
+  try {
+    const gear = await axios.delete(`${BASE_URL}/gear/${gearId}`);
+    return gear.data;
+    } catch (e) {
+      console.log(e.message);
+    };
+};
+
+export const updateGear = async (data,gearId) => {
+  const gear = await axios.put(`${BASE_URL}/gear/${gearId}`, data);
+  return gear.data;
+};
+
+export const createGear = async (data) => {
+  let res = await axios.post(`${BASE_URL}/gear`, data);
+  return res.data;
+}
