f07d7f81e40aa18d661c41d9792cdb1621764e84
darrinim <darrinj.im@gmail.com>
2019-09-16 14:39:12 -0400

    created location controller

---
+++ controllers/locationController.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const { Location } = require('../model');
+// check above if it doesn't work
+
+const locationController = Router();
+
+locationController.get('/location', async (req, res, next) => {
+  try {
+    const locations = await Location.findAll();
+    res.json(location);
+  } catch(e) {
+    next(e);
+  }
+});
+
+module.exports = locationController;
