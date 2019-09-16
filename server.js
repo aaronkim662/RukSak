029973c6f072a13866dbea072312ce72bbdabd77
Aaronkim662 <aaronkim662@gmail.com>
2019-09-15 21:02:00 -0400

    Added to preliminary work

--- server.js
+++ server.js
@@ -2,20 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-const PORT = process.env.PORT || 3001;
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-app.use((e, req, res) => {
-  if (e) {
-    console.log(e.message); // eslint-disable-line
-    res.status(500).send(e.message);
-  }
-});
+// app.use((e, req, res) => {
+//   if (e) {
+//     console.log(e.message); // eslint-disable-line
+//     res.status(500).send(e.message);
+//   }
+// });
 
 app.get('/', (req, res) => {
   res.send('Welcome to the default route. 3001');
