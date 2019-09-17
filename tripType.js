f545a082cc628724f917b2ba40e5eccb41caae99
Radiscipio <robertdiscipio@gmail.com>
2019-09-17 16:59:21 -0400

    Added photos and TripType data

--- tripType.js
+++ tripType.js
@@ -1,6 +1,14 @@
 let data =  [
   {
-    name: 'Camping'
-    image: ''
+    name: 'Camping',
+    image: 'client/src/images/Camping-banner.jpg'
+  }, {
+    name: 'Hiking',
+    image: 'client/src/images/Hiking-banner.jpg'
+  }, {
+    name: 'Biking',
+    image: 'client/src/images/Biking-banner.jpg'
   }
-]
+];
+
+export default data
