6a1691e14dd6ea47a0449bd29c0155399ea59ae0
darrinim <darrinj.im@gmail.com>
2019-09-17 09:54:50 -0400

    created Form structure

--- client/src/Component/Form/Form.js
+++ client/src/Component/Form/Form.js
@@ -1,16 +1,57 @@
 import React from 'react';
 
 class Form extends React.Component {
-  state = {
+  constructor(props) {
+    super(props)
+    this.state = {
 
+    }
   }
+
   render() {
     return (
-      <form>
-        This is form
-      </form>
+      <div className="formContainer">
+        <div className="returnUsers">
+          <h2>Returning Users</h2>
+          <form className="returningForm"
+            onSubmit={this.props.onSubmit}
+          >
+            <input
+              type="text"
+              placeholder="email"
+              onChange={this.props.handleChange}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              onChange={this.props.handleChange}
+            />
+          </form>
+        </div>
+        <div className="newUsers">
+          <h2>New Users</h2>
+          <form className="returningForm"
+            onSubmit={this.props.onSubmit}
+          >
+            <input
+              type="text"
+              placeholder="username"
+              onChange={this.props.handleChange}
+            />
+            <input
+              type="text"
+              placeholder="email"
+              onChange={this.props.handleChange}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              onChange={this.props.handleChange}
+            />
+          </form>
+        </div>
+      </div>
+
     )
   }
 }
-
-export default Form
