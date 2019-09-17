0e8bf0bdb9895cf7e5dca231be1d762afd55f04a
Aaronkim662 <aaronkim662@gmail.com>
2019-09-17 16:09:46 -0400

    Edits to plan

--- client/src/Component/planning/Planning.js
+++ client/src/Component/planning/Planning.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
-function Planning() {
-  return(
-    <div>This is Planning</div>
-  )
+class Planning extends React.Component {
+  state = {
+    days: "",
+    people: "",
+    location: "",
+  }
+
+  handleClick = (e) => {
+    e.preventDefault();
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="planning">
+          <div className="camping">
+            <h2>Camping</h2>
+            <div className="daysSelection"></div>
+            <div className="peopleSelection"></div>
+            <div className="recommended"></div>
+          </div>
+          <div className="itinerary"></div>
+            <div className="days"></div>
+            <div className="people"></div>
+            <div className="location"></div>
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 
 export default Planning
