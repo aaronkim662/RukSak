bfd1361a275540bd48240046b0f96c2e41ed1c5c
Radiscipio <robertdiscipio@gmail.com>
2019-09-18 16:55:22 -0400

    Added styling to home page

--- client/src/Component/Profile/Profile.js
+++ client/src/Component/Profile/Profile.js
@@ -6,12 +6,10 @@ function Profile() {
     <div className="profileContainer">
       <div className="profileImage"><i className="far fa-user-circle fa-6x"></i></div>
       <div className="profileInfo">
-        <h2 className="profileHeader">Profile Info</h2>
         <ul className="unorderedList">
           <li className="profileList">Username: </li>
           <li className="profileList">Email: </li>
         </ul>
-        <button className="editButton">Edit Info</button>
       </div>
       <div className="profileTrips">
         <h2 className="profileHeader">Current Trips</h2>
