2779fa4753064951b7a74b1958190b98205a031b
Radiscipio <robertdiscipio@gmail.com>
2019-09-18 14:32:09 -0400

    Added styling to home page

--- client/src/Component/TripChoice/TripChoice.js
+++ client/src/Component/TripChoice/TripChoice.js
@@ -6,29 +6,24 @@ import './TripChoice.css'
 function TripChoice() {
 
   return(
-    <div className="trip-choice">
-      <div>
-      <img
-      className='camping-photo'
-      src="https://i.imgur.com/vVIAXS0.jpg" alt='Camping'/>
+    <div>
+      <div className="trip-choice">
+        <img
+        className='photo'
+        src="https://i.imgur.com/vVIAXS0.jpg" alt='Camping'/>
+        <h2 className="trip-name">Camping</h2>
       </div>
       <div>
-        <h2>Camping</h2>
+        <img
+        className='photo'
+        src="https://i.imgur.com/GFZQ8B4.jpg" alt='Hiking'/>
+        <h2 className="trip-name">Hiking</h2>
       </div>
       <div>
-      <img
-      className='hiking-photo'
-      src="https://i.imgur.com/GFZQ8B4.jpg" alt='Camping'/>
-      </div>
-      <div>
-        <h2>Hiking</h2>
-      </div><div>
-      <img
-      className='biking-photo'
-      src="https://i.imgur.com/qVvoGj4.jpg" alt='Camping'/>
-      </div>
-      <div>
-        <h2>Biking</h2>
+        <img
+        className='photo'
+        src="https://i.imgur.com/qVvoGj4.jpg" alt='Biking'/>
+        <h2 className="trip-name">Biking</h2>
       </div>
     </div>
   )
