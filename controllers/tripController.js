3f8de6bef1c60719111e39e5f8db85984895560e
Aaronkim662 <aaronkim662@gmail.com>
2019-09-19 09:18:41 -0400

    Join tables

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -1,16 +1,24 @@
 const express = require('express');
 
-const { Trip, Gear } = require('../model');
+const { Trip, Gear, User } = require('../model');
 
 const tripController = express.Router();
 
 tripController.put('/:tripid/gears/:gearid/', async (req, res) => {
   try {
-    const tripGear = await Trip.findByPk(req.params.tripid);
-    const prevGear = await tripGear.getGears();
-    const newGear = await Gear.findByPk(req.params.gearid);
-    await tripGear.setGears([...prevGear, newGear]);
-    res.json({ ...tripGear, gears: [...prevGear, newGear] });
+    const tripGear = await Trip.findOne({
+      where: { id: req.params.tripid },
+      include: [
+        User, Gear,
+      ],
+    });
+    // update here
+    res.json(tripGear);
+    // const tripGear = await Trip.findByPk(req.params.tripid);
+    // const prevGear = await tripGear.getGears();
+    // const newGear = await Gear.findByPk(req.params.gearid);
+    // await tripGear.setGears([...prevGear, newGear]);
+    // res.json({ ...tripGear, gears: [...prevGear, newGear] });
   } catch (e) {
     res.status(500).send(e);
   }
